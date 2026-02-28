e664a5c66c04890fff497d4521d0cc5939a3bec1
Louis Geist <louisjjgeist@gmail.com>
2026-02-28 11:12:56 +0100

    dev(jz): num_worker

--- configs/s3dis/ptv3_nonormal.py
+++ configs/s3dis/ptv3_nonormal.py
@@ -2,7 +2,7 @@ _base_ = ["../_base_/default_runtime.py"]
 
 # misc custom setting
 batch_size = 3 #12  # bs: total bs in all gpus
-num_worker = 24
+num_worker = 4
 mix_prob = 0.8
 empty_cache = False
 enable_amp = True
