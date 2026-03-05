213c8a76e6be31ebdac437c7dc99f6721519829a
Louis Geist <louisjjgeist@gmail.com>
2026-03-05 19:55:14 +0100

    exps

---
+++ configs/experiment/w85/4/3b/5.py
@@ -0,0 +1,19 @@
+_base_ = ["./1.py"]
+
+# Exp 5: lr=6e-4, effective_bs=12
+lr = 6e-4
+batch_size_per_gpu = 3
+batch_size = batch_size_per_gpu * num_gpu
+
+optimizer = dict(type="AdamW", lr=lr, weight_decay=0.05)
+scheduler = dict(
+    type="OneCycleLR",
+    max_lr=[lr, lr / 10],
+    pct_start=0.05,
+    anneal_strategy="cos",
+    div_factor=10.0,
+    final_div_factor=1000.0,
+)
+param_dicts = [dict(keyword="block", lr=lr / 10)]
+
+wandb_run_name = f"Exp 3b.5 | effective_bs={batch_size}, lr={lr}, patch_size={patch_size}"
