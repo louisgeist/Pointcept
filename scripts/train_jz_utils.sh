c1ac27c2a77ee5c749f9a5ed925e9fdc58eff06b
Louis Geist <louisjjgeist@gmail.com>
2026-03-19 18:14:10 +0100

    fix(train_jz_utils.hs): incorrect DIST_URL (on A100 only, H100 was already fine)

--- scripts/train_jz_utils.sh
+++ scripts/train_jz_utils.sh
@@ -68,9 +68,13 @@ echo "Machine Num: $NUM_MACHINE"
 
 if [ -n "$SLURM_NODELIST" ]; then
   MASTER_HOSTNAME=$(scontrol show hostname "$SLURM_NODELIST" | head -n 1)
-  MASTER_ADDR=$(getent hosts "$MASTER_HOSTNAME" | awk '{ print $1 }')
+  MASTER_ADDR=$(getent hosts "$MASTER_HOSTNAME" | head -n 1 | awk '{ print $1 }')
   MASTER_PORT=$((10000 + 0x$(echo -n "${EXP_NAME}" | md5sum | cut -c 1-4 | awk '{print $1}') % 20000))
-  DIST_URL=tcp://$MASTER_ADDR:$MASTER_PORT
+  # IPv6 addresses need brackets in URL: tcp://[fe80::1]:port. Use first address only (getent can return multiple).
+  case "$MASTER_ADDR" in
+    *:*) DIST_URL="tcp://[${MASTER_ADDR}]:${MASTER_PORT}" ;;
+    *)   DIST_URL="tcp://${MASTER_ADDR}:${MASTER_PORT}" ;;
+  esac
 fi
 
 echo "Dist URL: $DIST_URL"
