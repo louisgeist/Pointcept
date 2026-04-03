3aca113b2e5c2295c85f33996963651c34c52a6c
Louis Geist <louisjjgeist@gmail.com>
2026-04-03 12:50:15 +0200

    exp litept check

---
+++ configs/experiment/w89/5/litept_mini/mini.py
@@ -0,0 +1,341 @@
+_base_ = ["../../../../_base_/default_runtime.py"] # level experiment/wXX/DD/subfolder/config.py
+
+# Configs
+# wandb_run_name -> to define at the end of the file
+
+exp_group = 3
+num_exp = 1
+# label definition: inter_finerall6 
+num_classes = 8
+ignore_index = 8
+grid_size = 0.1
+point_max = 102400 #LitePT : 102400 - test 204800 as in PTv3 ?
+
+
+num_gpu = 1
+epoch = 2
+eval_epoch = 1
+lr = 2e-3 #LitePT waymo : lr=0.002
+warmup_steps = 5000
+patch_size = 1024 #LitePT : 1024
+
+# Specific things I setted
+test_single_fragment = True
+tta = False # no TTA (cf. aug_transform)
+
+# misc custom setting
+batch_size_per_gpu = 16
+batch_size = batch_size_per_gpu * num_gpu
+num_worker = 8 * num_gpu
+mix_prob = 0.8
+empty_cache = False
+enable_amp = True
+
+wandb_run_name = f"LitePT {exp_group}.{num_exp}) eff_bs={batch_size} & point_max={point_max} | harmonized transforms | lr={lr} | grid_size={grid_size} | warmup_steps={warmup_steps} | input=RGB"
+
+# Hooks
+# Note: configs are imported as python modules before `_base_` is merged, so we
+# must redefine `hooks` here instead of mutating it.
+hooks = [
+    dict(type="CheckpointLoader"),
+    dict(type="ModelHook"),
+    dict(type="IterationTimer", warmup_iter=2),
+    dict(type="InformationWriter", log_interval=100, log_train_miou=True),
+    dict(type="SemSegEvaluator", write_cls_iou=True),
+    dict(type="CheckpointSaver", save_freq=3),
+    dict(type="PreciseEvaluator", test_last=False),
+]
+
+
+# model settings
+model = dict(
+    type="DefaultSegmentorV2",
+    num_classes=num_classes,
+    backbone_out_channels=72,
+    backbone=dict(
+    type="LitePT-v1",
+        in_channels=3, # RGB
+        order=("z", "z-trans", "hilbert", "hilbert-trans"),
+        stride=(2, 2, 2, 2),
+        enc_depths=(2, 2, 2, 6, 2),
+        enc_channels=(36, 72, 144, 252, 504),
+        enc_num_head=(2, 4, 8, 14, 28),
+        enc_patch_size=(patch_size, patch_size, patch_size, patch_size, patch_size),
+        enc_conv=(True, True, True, False, False),
+        enc_attn=(False, False, False, True, True),
+        enc_rope_freq=(100.0, 100.0, 100.0, 100.0, 100.0),
+        dec_depths=(0, 0, 0, 0),
+        dec_channels=(72, 72, 144, 252),
+        dec_num_head=(4, 4, 8, 14),
+        dec_patch_size=(patch_size, patch_size, patch_size, patch_size),
+        dec_conv=(False, False, False, False),
+        dec_attn=(False, False, False, False),
+        dec_rope_freq=(100.0, 100.0, 100.0, 100.0),
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        attn_drop=0.0,
+        proj_drop=0.0,
+        drop_path=0.3,
+        shuffle_orders=True,
+        pre_norm=True,
+        enc_mode=False,
+    ),
+    criteria=[
+        dict(type="CrossEntropyLoss", loss_weight=1.0, ignore_index=ignore_index),
+        dict(type="LovaszLoss", mode="multiclass", loss_weight=1.0, ignore_index=ignore_index),
+    ],
+)
+
+# scheduler settings
+optimizer = dict(type="AdamW", lr=lr, weight_decay=0.005)
+# scheduler = dict(
+#     type="OneCycleLR",
+#     max_lr=[lr, lr/10],
+#     pct_start=0.05,
+#     anneal_strategy="cos",
+#     div_factor=10.0,
+#     final_div_factor=1000.0,
+# )
+scheduler = dict(type="LinearLR", 
+                 start_factor = 1/10, # start with lr/10
+                 total_iters = warmup_steps, # number of steps before reaching lr (and plateauing) 
+                )
+param_dicts = [dict(keyword="block", lr=lr/10)]
+
+# dataset settings
+dataset_type = "Flair3DDataset"
+data_root = "data/flair3d"
+
+data = dict(
+    num_classes=num_classes,
+    ignore_index=ignore_index,
+    names=[
+        "Building",
+        "Greenhouse",
+        "Impervious surface",
+        "Other soil",
+        "Herbaceous",
+        "Vineyard",
+        "Tree",
+        "Other infrastructure",
+        # "Agricultural soil", # label definition: inter_finerall7 
+        "Void",
+    ],
+    train=dict(
+        type=dataset_type,
+        split="train",
+        data_root=data_root,
+        transform=[
+            dict(type="CenterShift", apply_z=True),
+            dict(type="RandomDropout", dropout_ratio=0.2, dropout_application_ratio=0.2),
+            dict(type="RandomRotate", angle=[-1, 1], axis="z", center=[0, 0, 0], p=0.5), # as waymo/nuscences
+            dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="x", p=0.5),
+            dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="y", p=0.5),
+            dict(type="RandomScale", scale=[0.9, 1.1]),                                 # as waymo/nuscences
+            dict(type="RandomFlip", p=0.5),                                             # as waymo/nuscences
+            dict(type="RandomJitter", sigma=0.005, clip=0.02),                          # as waymo/nuscences
+            dict(type="ChromaticAutoContrast", p=0.2, blend_factor=None),
+            dict(type="ChromaticTranslation", p=0.95, ratio=0.05),
+            dict(type="ChromaticJitter", p=0.95, std=0.05),
+            
+            dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="train",
+                return_grid_coord=True,
+            ), # as waymo/nuscences
+            
+            dict(type="SphereCrop", point_max=point_max, mode="random"),
+            dict(type="CenterShift", apply_z=False),
+            dict(type="NormalizeColor"),
+            
+            dict(type="ToTensor"),                                                      # as waymo/nuscences
+            dict(type="Update", keys_dict={"grid_size": grid_size}),                    # as waymo/nuscences
+            dict(
+                type="Collect",
+                keys=("coord", "grid_coord", "segment", "grid_size"),
+                feat_keys=["color"], # "normal"), # compared to LitePT, remove "normal"
+            ),                                                                          # as waymo/nuscences
+        ],
+        test_mode=False,
+    ),
+    val=dict(
+        type=dataset_type,
+        split="val",        
+        data_root=data_root,
+        transform=[
+            dict(type="CenterShift", apply_z=True),
+            dict(type="Copy", keys_dict={"segment": "origin_segment"}),
+            dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="train",
+                return_grid_coord=True,
+                return_inverse=True,
+            ),
+            dict(type="CenterShift", apply_z=False),
+            dict(type="NormalizeColor"),
+            dict(type="ToTensor"),
+            dict(
+                type="Collect",
+                keys=("coord", "grid_coord", "segment", "origin_segment", "inverse"),
+                feat_keys=["color"], # "normal"),
+            ),
+        ],
+        test_mode=False,
+    ),
+    test=dict(
+        type=dataset_type,
+        split="test",
+        data_root=data_root,
+        transform=[
+            dict(type="CenterShift", apply_z=True),
+            dict(type="NormalizeColor"),
+        ],
+        test_mode=True,
+        test_cfg=dict(
+            voxelize=dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="test",
+                return_grid_coord=True,
+                test_single_fragment=test_single_fragment,
+            ),
+            crop=None,
+            post_transform=[
+                dict(type="CenterShift", apply_z=False),
+                dict(type="ToTensor"),
+                dict(
+                    type="Collect",
+                    keys=("coord", "grid_coord", "index"),
+                    optional_keys=("inverse",),  # for test_single_fragment broadcast
+                    feat_keys=["color"], # "normal"),
+                ),
+            ],
+            aug_transform=[
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[0],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    )
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    )
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    )
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[3 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    )
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[0],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[0.95, 0.95]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[0.95, 0.95]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[0.95, 0.95]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[3 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[0.95, 0.95]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[0],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[1.05, 1.05]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[1.05, 1.05]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[1],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[1.05, 1.05]),
+                ],
+                [
+                    dict(
+                        type="RandomRotateTargetAngle",
+                        angle=[3 / 2],
+                        axis="z",
+                        center=[0, 0, 0],
+                        p=1,
+                    ),
+                    dict(type="RandomScale", scale=[1.05, 1.05]),
+                ],
+                [dict(type="RandomFlip", p=1)],
+            ] if tta else [[dict(type="RandomRotateTargetAngle", angle=[0], axis="z", center=[0, 0, 0], p=1)]],
+        ),
+    ),
+)
+
