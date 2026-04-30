36d0655effde80272260e87122494f0fada3fc99
Louis Geist <louisjjgeist@gmail.com>
2026-04-30 12:43:08 +0200

    exps: h3d train longer

---
+++ configs/experiment/w93/4/h3d_2/spunet_3.py
@@ -0,0 +1,210 @@
+_base_ = ["../../../../_base_/default_runtime.py"] 
+
+num_gpu = 1
+grp_exp = 2
+num_exp = 3
+
+num_worker = 8 * num_gpu
+
+num_classes = 11
+ignore_index = num_classes
+
+tta = False
+test_single_fragment = True
+
+# misc custom setting
+batch_size = 24 * num_gpu
+mix_prob = 0.8
+empty_cache = False
+enable_amp = False
+
+lr = 5e-3
+warmup_steps = 2500
+grid_size = 0.1
+point_max = 100000
+
+epoch = 800
+eval_epoch = epoch//10
+feat_keys = ["coord", "color"]
+
+# dataset settings
+dataset_type = "H3DDataset"
+data_root = "data/h3d"
+
+wandb_run_name = f"SpUNet on {dataset_type[:-7]}: {grp_exp}.{num_exp}) lr={lr}, epoch={epoch}, bs={batch_size}"
+wandb_project = f"pointcept_{dataset_type[:-7].lower()}"
+
+# model settings
+model = dict(
+    type="DefaultSegmentor",
+    backbone=dict(
+        type="SpUNet-v1m1",
+        in_channels=6,  # xyz + RGB
+        num_classes=num_classes,
+        channels=(32, 64, 128, 256, 256, 128, 96, 96),
+        layers=(2, 3, 4, 6, 2, 2, 2, 2),
+    ),
+    criteria=[dict(type="CrossEntropyLoss", loss_weight=1.0, ignore_index=ignore_index)],
+)
+
+# scheduler settings
+optimizer = dict(type="AdamW", lr=lr, weight_decay=0.005)
+scheduler = dict(
+    type="OneCycleLR",
+    max_lr=optimizer["lr"],
+    pct_start=0.05,
+    anneal_strategy="cos",
+    div_factor=10.0,
+    final_div_factor=10000.0,
+)
+# scheduler = dict(type="LinearLR", 
+#                  start_factor = 1/10, # start with lr/10
+#                  total_iters = warmup_steps, # number of epochs before reaching lr (and plateauing) 
+#                  # As we have 100 epoch for small training:
+#                  # 1. 50 epochs of linear increase, 
+#                  # 2. then 50 epochs of constant lr
+#                 )
+
+hooks = [
+    dict(type="CheckpointLoader"),
+    dict(type="ModelHook"),
+    dict(type="IterationTimer", warmup_iter=2),
+    dict(type="InformationWriter", log_interval=100),
+    dict(type="SemSegEvaluator", write_cls_iou=True),
+    dict(type="CheckpointSaver", save_freq=None),
+    dict(type="PreciseEvaluator", test_last=False),
+]
+
+data = dict(
+    num_classes=num_classes,
+    ignore_index=ignore_index,
+    names=[
+        "Low Vegetation",
+        "Impervious Surface",
+        "Vehicle",
+        "Urban Furniture",
+        "Roof",
+        "Façade",
+        "Shrub",
+        "Tree",
+        "Soil or Gravel",
+        "Vertical Surface",
+        "Chimney",
+        
+        "Void",
+        ],
+    train=dict(
+        type=dataset_type,
+        split="train",
+        data_root=data_root,
+        transform=[
+            dict(type="CenterShift", apply_z=True),
+            dict(type="RandomDropout", dropout_ratio=0.2, dropout_application_ratio=0.2),
+            dict(type="RandomRotate", angle=[-1, 1], axis="z", center=[0, 0, 0], p=0.5),
+            # dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="x", p=0.5),
+            # dict(type="RandomRotate", angle=[-1 / 64, 1 / 64], axis="y", p=0.5),
+            dict(type="RandomScale", scale=[0.9, 1.1]),
+            dict(type="RandomFlip", p=0.5),
+            dict(type="RandomJitter", sigma=0.005, clip=0.02),
+            dict(type="ChromaticAutoContrast", p=0.2, blend_factor=None),
+            dict(type="ChromaticTranslation", p=0.95, ratio=0.05),
+            dict(type="ChromaticJitter", p=0.95, std=0.05),
+            dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="train",
+                return_grid_coord=True,
+            ),
+            dict(type="SphereCrop", point_max=point_max, mode="random"),
+            dict(type="CenterShift", apply_z=False),
+            dict(type="NormalizeColor"),
+            dict(type="ShufflePoint"), # might be worth to test
+            dict(type="ToTensor"),
+            dict(
+                type="Collect",
+                keys=("coord", "grid_coord", "segment"),
+                feat_keys=feat_keys,
+            ),
+        ],
+        test_mode=False,
+    ),
+    val=dict(
+        type=dataset_type,
+        split="val",
+        data_root=data_root,
+        transform=[
+            dict(type="CenterShift", apply_z=True),
+            dict(type="Copy", keys_dict={"segment": "origin_segment"}),
+            dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="train",
+                return_grid_coord=True,
+                return_inverse=True,
+            ),
+            dict(type="CenterShift", apply_z=False),
+            dict(type="NormalizeColor"),
+            dict(type="ToTensor"),
+            dict(
+                type="Collect",
+                keys=("coord", "grid_coord", "segment", "origin_segment", "inverse"),
+                feat_keys=feat_keys,
+            ),
+        ],
+        test_mode=False,
+    ),
+    test=dict(
+        type=dataset_type,
+        split="test",
+        data_root=data_root,
+        transform=[dict(type="CenterShift", apply_z=True), dict(type="NormalizeColor")],
+        test_mode=True,
+        test_cfg=dict(
+            voxelize=dict(
+                type="GridSample",
+                grid_size=grid_size,
+                hash_type="fnv",
+                mode="test",
+                return_grid_coord=True,
+                test_single_fragment=test_single_fragment,
+            ),
+            crop=None,
+            post_transform=[
+                dict(type="CenterShift", apply_z=False),
+                dict(type="ToTensor"),
+                dict(
+                    type="Collect",
+                    keys=("coord", "grid_coord", "index"),
+                    optional_keys=("inverse",),  # for test_single_fragment broadcast
+                    feat_keys=feat_keys,
+                ),
+            ],
+            aug_transform=[
+                [dict(type="RandomScale", scale=[0.9, 0.9])],
+                [dict(type="RandomScale", scale=[0.95, 0.95])],
+                [dict(type="RandomScale", scale=[1, 1])],
+                [dict(type="RandomScale", scale=[1.05, 1.05])],
+                [dict(type="RandomScale", scale=[1.1, 1.1])],
+                [
+                    dict(type="RandomScale", scale=[0.9, 0.9]),
+                    dict(type="RandomFlip", p=1),
+                ],
+                [
+                    dict(type="RandomScale", scale=[0.95, 0.95]),
+                    dict(type="RandomFlip", p=1),
+                ],
+                [dict(type="RandomScale", scale=[1, 1]), dict(type="RandomFlip", p=1)],
+                [
+                    dict(type="RandomScale", scale=[1.05, 1.05]),
+                    dict(type="RandomFlip", p=1),
+                ],
+                [
+                    dict(type="RandomScale", scale=[1.1, 1.1]),
+                    dict(type="RandomFlip", p=1),
+                ],
+            ] if tta else [[dict(type="RandomRotateTargetAngle", angle=[0], axis="z", center=[0, 0, 0], p=1)]],
+        ),
+    ),
+)
